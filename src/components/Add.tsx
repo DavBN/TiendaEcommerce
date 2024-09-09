2a0c0810535b0473d9cba301b36c3069deabdeb6
DavBN <162839191+DavBN@users.noreply.github.com>
2024-09-09 16:01:20 -0500

    Se editó los dos componentes creados para customizar el producto y para agregar una cantidad x de producto, por el momento la funcionalidad de agregar es local a modo de prueba, funciona correctamente, cada componente nuevo tambien es totalmente responsivo.
    Commit 2

--- src/components/Add.tsx
+++ src/components/Add.tsx
@@ -1,9 +1,46 @@
-import React from 'react'
+"use client"; {/*Componente del lado de uso del cliente */ }
+import React, { useState } from 'react'; {/*Importación del hook usestate */ }
 
+
+{/*Componente Add */ }
 export const Add = () => {
-  return (
-    <div>Add</div>
-  )
+    const [quantity, setQuantity] = useState(1); {/*Uso del hook dentro de la constante para la cantidad de productos */ }
+
+    {/*Constante temporal para probar funcionalidad */ }
+    const stock = 4;
+
+    {/*Función para el aumento o disminución de cantidad, disminuye solo si es mayor que 1 y aumenta la cantidad si solo es menor que el stock disponible */ }
+    const handleQuantity = (type: "i" | "d") => {
+        if (type === "d" && quantity > 1) {
+            setQuantity((prev) => prev - 1); {/*I incrementa */ }
+        }
+        if (type === "i" && quantity < stock) {
+            setQuantity((prev) => prev + 1); {/*D Disminute */ }
+        }
+    };
+
+    return (
+        <div className='flex flex-col gap-4'> {/*Div contenedor principal */}
+            <h4 className='font-medium'>Elige la cantidad</h4> {/*Título de la sección para selección de cantidad de producto */}
+            <div className='flex justify-between'>
+                <div className='flex items-center gap-4'>
+                    <div className='bg-gray-100 py-2 px-4 rounded-3xl flex items-center justify-between w-32'>
+                        <button className='cursor-pointer text-xl' onClick={() => handleQuantity("d")}>-</button>
+                        {quantity}
+                        <button className='cursor-pointer text-xl' onClick={() => handleQuantity("i")}>+</button>
+                    </div>
+                    <div className='text-xs'>
+                        Solo <span className='text-orange-500'>4 bicicletas</span> a tu izquierda <br /> {"No lo"}{" "} dejes pasar
+                    </div>
+                </div>
+
+                {/*Botón con función de hover para que al pasar el mouse se rellene de rojo */}
+                <button className='w-36 text-sm rounded-3xl ring-1 ring-tienda text-red-500 py-2 px-4
+         hover:bg-tienda hover:text-white disabled:cursor-not-allowed disabled:bg-red-300 disabled:text-white disabled:ring-none'>Agregar al carro
+                </button>
+            </div>
+        </div>
+    )
 }
 
 export default Add
--- src/components/CustomizeProducts.tsx
+++ src/components/CustomizeProducts.tsx
@@ -1,9 +1,33 @@
 import React from 'react'
 
+{/*Componente de CustomizeProducts */ }
 const CustomizeProducts = () => {
-  return (
-    <div>CustomizeProducts</div>
-  )
+    return (
+        <div className='flex flex-col gap-6'> {/*Div padre */}
+            <h4 className='font-medium'>Elige el color de tu bici</h4> {/*Título para mostrar descripción de la sección */}
+            <ul className='flex items-center gap-3'> {/*Lista para ordenar los colores a escoger */}
+                <li className='w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-red-600'>{/*Colores a seleccionar */}
+                    <div className="absolute w-10 h-10 rounded-full ring-2 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />
+                </li>
+                <li className='w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-pointer relative bg-green-600'></li>{/*Colores a seleccionar */}
+                <li className='w-8 h-8 rounded-full ring-1 ring-gray-300 cursor-not-allowed relative bg-blue-600'>{/*Función de color no disponible o no existe */}
+                    <div className="absolute w-10 h-[2px] bg-red-400 rotate-45 top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2" />{/*Linea roja en la mitad del circulo */}
+                </li>
+            </ul>
+            <h4 className='font-medium'>Elige el tamaño de tu bici</h4>{/*Título para mostrar descripción de la sección */}
+            <ul className='flex items-center gap-3'>{/*Lista para escoger los tamaños */}
+                <li className='ring-1 ring-gray-200  text-red rounded-md py-1 px-4 text-sm cursor-pointer'> {/*Seleccióbn 1 */}
+                    Pequeña
+                </li>
+                <li className='ring-1 ring-gray-200  text-white  bg-tienda rounded-md py-1 px-4 text-sm cursor-pointer'> {/*Seleccióbn 2  */}
+                    Mediana
+                </li>
+                <li className='ring-1 ring-gray-200  text-white bg-red-300 rounded-md py-1 px-4 text-sm cursor-not-allowed'> {/*Seleccióbn 3 no disponible */}
+                    Grande
+                </li>
+            </ul>
+        </div>
+    )
 }
 
 export default CustomizeProducts
