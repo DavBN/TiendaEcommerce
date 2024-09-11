82003c9d835ebb6402c8afbaa8461be922df3261
DavBN <162839191+DavBN@users.noreply.github.com>
2024-09-11 13:09:51 -0500

    Revisión del error por parte de la función, se colocó dentro de un try catch por el momento para poder la page y continuar para luego válidar nuevamente el error.
    Commit 1

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -28,7 +28,7 @@ const Home = async () => {
       <div className="mt-24  px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl">Productos</h1>     {/*Sección prodductos */}
         <Suspense fallback={"loadin"}>
-        <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} /> {/*Lista de productos */}
+        <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} limit={4}/> {/*Lista de productos */}
         </Suspense>
       </div>
       <div className="mt-24">
@@ -37,7 +37,7 @@ const Home = async () => {
       </div>
       <div className="mt-24  px-4 md:px-8 lg:px-16 xl:32 2xl:px-64">
         <h1 className="text-2xl">Nuevos productos</h1>   {/*Sección Nuevos productos */}
-        <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} /> {/*Lista de productos */}
+        <ProductList categoryId={process.env.FEATURED_PRODUCTS_CATEGORY_ID!} limit={4} /> {/*Lista de productos */}
       </div>
     </main>
   );
--- src/components/ProductList.tsx
+++ src/components/ProductList.tsx
@@ -1,3 +1,4 @@
+import { WixClientContext } from "@/context/WixContext";
 import { wixClientServer } from "@/lib/wixClientServer";
 import { products } from "@wix/stores";
 import Image from "next/image"; {/*Importación de next image para el uso de imagenes */ }
@@ -13,25 +14,36 @@ const ProductList = async ({
     categoryId: string;
     limit?: number;
 }) => {
+    let res;
+    try {
+        const wixClient = await wixClientServer();
 
-    const wixClient = await wixClientServer();
-
-    const res = await wixClient.products
-    .queryProducts()
-    .eq("collectionIds", categoryId)
-    .limit(limit || PRODUCT_PER_PAGE)
-    .find(); {/*Llama el método e inicia una consulta de productos */ }
+        res = await wixClient.products
+            .queryProducts()
+            .eq("collectionIds", categoryId)
+            .limit(limit || PRODUCT_PER_PAGE)
+            .find();
+    } catch (error) {
+        console.error("Erro", error);
+        return (
+            <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap">
+                <p>No hay productos para mostrar.</p>
+            </div>
+        );
+    }
     return (
         <div className="mt-12 flex gap-x-8 gap-y-16 justify-between flex-wrap"> {/*Div padre */}
-            {res.items.map((product:products.Product) => (
+            {res.items.map((product: products.Product) => (
                 < Link href={"/" + product.slug} className="w-full flex flex-col gap-4 sm:w-[45%] lg:w-[22%]" key={product._id} >
                     <div className="relative w-full h-80">
-                        <Image src="/bici.jpg" alt="Bici" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
-                        <Image src="/bici1.jpg" alt="Bici" fill sizes="25vw" className="absolute object-cover rounded-md" />
+                        <Image src={product.media?.mainMedia?.image?.url || "/product.png"} alt="Bici" fill sizes="25vw" className="absolute object-cover rounded-md z-10 hover:opacity-0 transition-opacity easy duration-500" />
+                        {product.media?.items && (
+                            <Image src={product.media?.items[1]?.image?.url || "/product.png"} alt="algo" fill sizes="25vw" className="absolute object-cover rounded-md" />)}
+
                     </div>
                     <div className="flex justify-between">{/*Información del contenido mostrado */}
-                        <span className="font-medium">Nombre producto</span>{/*Información del contenido mostrado */}
-                        <span className="font-semibold">$700</span>{/*Información del contenido mostrado */}
+                        <span className="font-medium">{product.name}</span>{/*Información del contenido mostrado */}
+                        <span className="font-semibold">{product.price?.price}</span>{/*Información del contenido mostrado */}
                     </div>
                     <div className="text-sm text-gray-500">Descripción producto</div>
                     {/*Botón que al pasar el ratón o hacer click de rellena de color rojo */}
